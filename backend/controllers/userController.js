e16800ad114561fc97930ddd6eca9719debad903
Jonathan Kong <jonathankong96@hotmail.com>
2022-06-01 16:50:44 +0800

    if User is created

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -18,6 +18,34 @@ const registerUser = asyncHandler(async (req, res) => {
       throw new Error('Please include fields')
    }
 
+   // Find if user already exists
+   const userExists = await User.findOne({email})
+
+   if(userExists) {
+      res.status(400)
+      throw new Error('User already exists')
+   }
+
+   // Hash password
+   const salt = await bcrypt.genSalt(10)
+   const hashedPassword = await bcrypt.hash(password, salt)
+
+   // Create user
+   const user = await User.create({
+      name, 
+      email,
+      password: hashedPassword
+   })
+
+   // If useris created
+   if(user){
+      res.status(201).json({
+         _id: user._id,
+         name: user.name,
+         email: user.email
+      })
+   }
+
    res.send('Register Route')
 })
 
