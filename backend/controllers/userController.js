d75adccf3afcfd85bbda8d2386375b7bf4011655
Jonathan Kong <jonathankong96@hotmail.com>
2022-06-02 20:58:04 +0800

    able to GET from user/me

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -77,7 +77,7 @@ const loginUser = asyncHandler(async (req, res) => {
 // @route   /api/users/me
 // @access  Private
 const getMe = asyncHandler(async (req, res) => {
-   res.send('me')
+   res.status(200).json(req.user)
 })
 
 
