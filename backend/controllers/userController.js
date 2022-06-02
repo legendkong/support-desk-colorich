9e4ebd391c74c7a6583ae073ffb1d4ab861f595e
Jonathan Kong <jonathankong96@hotmail.com>
2022-06-02 20:52:49 +0800

    protect Fn to protect routes :bomb:

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -73,6 +73,15 @@ const loginUser = asyncHandler(async (req, res) => {
    }
 })
 
+// @desc    Get current user
+// @route   /api/users/me
+// @access  Private
+const getMe = asyncHandler(async (req, res) => {
+   res.send('me')
+})
+
+
+
 // Generate token
 const generateToken = (id) => {
    return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -83,4 +92,5 @@ const generateToken = (id) => {
 module.exports = {
    registerUser,
    loginUser,
+   getMe,
 }
