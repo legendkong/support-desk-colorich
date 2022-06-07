c4b09840d76bed99b287f83067d0345b0c5a7f63
Jonathan Kong <jonathankong96@hotmail.com>
2022-06-07 17:41:15 +0800

    name + email + password form

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,9 +1,68 @@
+import {useState} from 'react'
+import {FaUser} from 'react-icons/fa'
 
 function Register() {
+   const [formData, setFormdata] = useState({
+      name: '',
+      email: '',
+      password: '',
+      password2: ''
+   })
+
+   // Destructuring
+   const {name, email, password, password2} = formData
+
+   const onChange = (e) => {
+      setFormdata((prevState) => ({
+         ...prevState,
+         [e.target.name]: e.target.value,
+      }))
+   }
+
    return (
-      <div>
-         <h1>Register</h1>
-      </div>
+      <>
+         <section className="heading">
+            <h1>
+               <FaUser /> Register
+            </h1>
+            <p>Please create an account</p>
+         </section>
+
+         <section className="form">
+            <form>
+               <div className="form-group">
+                  <input 
+                  type="text" 
+                  className='form-control' 
+                  id='name' 
+                  name = 'name'
+                  value={name} 
+                  onChange={onChange} 
+                  placeholder='Enter your name' />
+               </div>
+               <div className="form-group">
+                  <input 
+                  type="email" 
+                  className='form-control' 
+                  id='email' 
+                  name = 'email'
+                  value={email} 
+                  onChange={onChange} 
+                  placeholder='Enter your email' />
+               </div>
+               <div className="form-group">
+                  <input 
+                  type='password'
+                  className='form-control' 
+                  id='password' 
+                  name = 'password'
+                  value={password} 
+                  onChange={onChange} 
+                  placeholder='Enter password' />
+               </div>
+            </form>
+         </section>
+      </>
    )
 }
 
