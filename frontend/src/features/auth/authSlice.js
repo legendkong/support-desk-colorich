f6e966db205c60f95292c2a4467d46bb32e5e97b
Jonathan Kong <jonathankong96@hotmail.com>
2022-06-08 23:14:52 +0800

    Axios to retrieve data(register)

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,4 +1,5 @@
 import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
+import authService from './authService'
 
 const initialState = {
    user: null,
@@ -11,7 +12,14 @@ const initialState = {
 // createAsyncThunk - Register new user
 export const register = createAsyncThunk('auth/register', 
 async(user, thunkAPI) => {
-   console.log(user)
+   try {
+      return await authService.register(user)
+   } catch (error) {
+      const message = (error.response && error.response.data && error.response.data.message)
+      || error.message || error.toString()
+
+      return thunkAPI.rejectWithValue(message)
+   }
    }
 )
 // createAsyncThunk - Login user
