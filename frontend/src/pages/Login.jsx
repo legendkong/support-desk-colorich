7a9b564df5a19c3e57ce4afa6e171b58df231614
Jonathan Kong <jonathankong96@hotmail.com>
2022-06-11 00:11:04 +0800

    Login.jsx -> useEffect hook

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import {FaSignInAlt} from 'react-icons/fa'
 import {toast} from 'react-toastify'
 import {useNavigate} from 'react-router-dom'
@@ -19,6 +19,19 @@ function Login() {
    const navigate = useNavigate()
 
    const {user, isLoading, isError, isSuccess, message} = useSelector(state => state.auth)
+
+   useEffect(() => {
+      if (isError) {
+         toast.error(message)
+      }
+
+      // Redirect when logged in
+      if (isSuccess || user) {
+         navigate('/')
+      }
+
+      dispatch(reset())
+   }, [isError, isSuccess, user, message, navigate, dispatch])   
 
 
    const onChange = (e) => {
